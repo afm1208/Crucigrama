d3534caf316f8f1806b812a7cc16a4fd36d521b9
User <User@PATICO>
2024-03-18 20:04:54 -0500

    prueba

---
+++ Model/Casilla.java
@@ -0,0 +1,28 @@
+
+/**
+ * 
+ */
+public class Casilla {
+
+    /**
+     * Default constructor
+     */
+    public Casilla() {
+    }
+
+    /**
+     * 
+     */
+    public int Fila;
+
+    /**
+     * 
+     */
+    public int Columna;
+
+    /**
+     * 
+     */
+    public boolean Estado;
+
+}
