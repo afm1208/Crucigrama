15758318bdc6b1748362f97ddc9ff3204f7452fd
Jorge Perdomo <joserrano4@poligran.edu.co>
2024-04-01 20:07:28 -0500

    Se suben librerias y calse inicial para la vista del juego

---
+++ CrosswordGame/src/com/poli/practica/project/view/PrincipalInterfaceView.java
@@ -0,0 +1,13 @@
+package com.poli.practica.project.view;
+
+import javafx.application.Application;
+
+public class PrincipalInterfaceView {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
